4a8638c9c5d013601bd79319a20d316aa2e33942
feyiErnestina <feyiernesyina1@mail.com>
2022-06-06 15:02:18 +0000

    javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+// variables
+//key words for variable ; let, const and var
+//var for global variables
+
+let randomNumber =Math.floor(math.random() * 100)+1;
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount=1;
+let resetButton;
+
+
+
+
+
